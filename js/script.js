29a8c835004f84c60e0b7c1a5af03e22e05c29fd
burillu <burillu90@hotmail.it>
2023-10-11 23:57:11 +0200

    created element div.container

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+/* 
+<div class="row">
+    <div class="col"></div>
+</div>
+ */
+
+// const containerFizz = document.getElementById('fizz-buzz');
+const divContainer= document.createElement('div');
+divContainer.className= 'container';
+console.log(divContainer);
+// prepend container in body
+//append row
+//aggiungere append col nel ciclo al posto del console.log
+
+
+
+
 for (let i = 1; i <= 100; i++) {
     if(i % 3=== 0 && i % 5===0){
         console.log('FizzBuzz')
