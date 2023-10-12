04ca6164671be52cfdb59f4fb00fb503db17b71a
burillu <burillu90@hotmail.it>
2023-10-12 11:24:03 +0200

    remove console.logs

--- js/script.js
+++ js/script.js
@@ -13,8 +13,6 @@ divRow.className= 'row';
 divContainer.append(divRow);
 // const divCol = document.createElement('div');
 
-console.log(divContainer);
-body.prepend(divContainer);
 // prepend container in body
 //append row
 //aggiungere append col nel ciclo al posto del console.log
@@ -25,27 +23,23 @@ let text, bg;
 for (let i = 1; i <= 100; i++) {
     const divCol = document.createElement('div');
     if(i % 3=== 0 && i % 5===0){
-        console.log('FizzBuzz');
         bg= ' bg-success';
         text= 'FizzBuzz';
     }else if ( i % 3 === 0){
-        console.log('Fizz');
         bg= ' bg-primary';
         text= 'Fizz';
     }else if ( i % 5 === 0){
-        console.log('Buzz');
         bg= ' bg-danger';
         text= 'Buzz';
     }else{
-        console.log(i);
         bg= ' bg-secondary';
         text= i;
     }
     //append
     divCol.classList='col'+ bg;
-    console.log(divCol);
     divCol.innerText= text
     divRow.append(divCol);
 
 
 }
+body.prepend(divContainer);
