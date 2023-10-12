f781cd911a45046250fee00e269695c478ccdac2
burillu <burillu90@hotmail.it>
2023-10-12 11:17:44 +0200

    for iter work and writing in html

--- js/script.js
+++ js/script.js
@@ -11,10 +11,10 @@ divContainer.className= 'container';
 const divRow = document.createElement('div');
 divRow.className= 'row';
 divContainer.append(divRow);
-const divCol = document.createElement('div');
-divCol.className= 'col';
+// const divCol = document.createElement('div');
 
 console.log(divContainer);
+body.prepend(divContainer);
 // prepend container in body
 //append row
 //aggiungere append col nel ciclo al posto del console.log
@@ -23,23 +23,29 @@ let text, bg;
 
 
 for (let i = 1; i <= 100; i++) {
+    const divCol = document.createElement('div');
     if(i % 3=== 0 && i % 5===0){
         console.log('FizzBuzz');
-        bg= 'my-bg-green';
+        bg= ' bg-success';
         text= 'FizzBuzz';
     }else if ( i % 3 === 0){
         console.log('Fizz');
-        bg= 'my-bg-blue';
+        bg= ' bg-primary';
         text= 'Fizz';
     }else if ( i % 5 === 0){
         console.log('Buzz');
-        bg= 'my-bg-yellow';
+        bg= ' bg-danger';
         text= 'Buzz';
     }else{
         console.log(i);
-        bg= 'my-bg-green';
-        text= 'Fizz';
+        bg= ' bg-secondary';
+        text= i;
     }
     //append
-    
+    divCol.classList='col'+ bg;
+    console.log(divCol);
+    divCol.innerText= text
+    divRow.append(divCol);
+
+
 }
